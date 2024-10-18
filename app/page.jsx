3bf2fce2ee4c89c49776ccb3fb81688ae0598f01
Alex Berkowitz <alexander.berkowitz@uscellular.com>
2024-10-18 15:53:57 -0500

    defaults

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <div>
+    <main>
       <h1>Alex Berkowitz</h1>
       <a>Designer</a>
       <a>Developer</a>
       <a>Maker</a>
-    </div>
+    </main>
   );
 }
