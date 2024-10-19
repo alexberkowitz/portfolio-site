de6df96f324e28498c616430b60a773db9986569
Alex Berkowitz <berky93@gmail.com>
2024-10-19 15:40:18 -0500

    update functions, add cursor, update layout

--- app/components/Background/Background.jsx
+++ app/components/Background/Background.jsx
@@ -11,7 +11,6 @@ const Background = (props) => {
   const drawing = useRef(true); // Whether or not to draw circles
   const lineWidth = useRef(300); // In px
   const pointMaxAge = 10; // In frames
-  const pixelDensity = 2; // Number of screen pixels each canvas pixel occupies
   
   // Initial setup
   useEffect(() => {
@@ -39,7 +38,7 @@ const Background = (props) => {
     new p5(p => {
       p.setup = () => {
         p.createCanvas(Math.floor(window.innerWidth), Math.floor(window.innerHeight)).parent(renderRef.current);
-        p.pixelDensity(1 / pixelDensity);
+        p.pixelDensity(1 / props.pixelDensity);
 
         // When the window resizes, update the drawing parameters
         window.addEventListener("resize", () => {
@@ -50,74 +49,114 @@ const Background = (props) => {
 
       p.draw = () => {
         p.frameRate(30);
+
+        // Draw the background
         p.background(255,255,255);
         
-        if( drawing.current ){
-          points.current.push({
-            x: p.mouseX,
-            y: p.mouseY,
-            age: 0
-          });
-        }
-
-        p.strokeWeight(lineWidth.current);
-
-        // Loop through the points array and draw each one
-        let prevPoint = {};
-        points.current.forEach((point, i) => {
-          if( point.age <= pointMaxAge ){
-            // Draw a line connecting each point to the previous one
-            if( i > 0 ){
-              const value = 255 * (point.age / pointMaxAge);
-              p.stroke(value, value, value);
-              p.line(prevPoint.x, prevPoint.y, point.x, point.y);
-            }
-            prevPoint = point;
-
-          } else {
-            delete points.current[i]; // Delete the circle when it gets too old
-          }
-
-          point.age += 1;
-        });
-        p.filter(p.BLUR, lineWidth.current / 4);
-
-        const normalizedWidth = Math.floor(p.width / pixelDensity);
+        // Draw the cursor trail
+        drawCursorTrail(p);
 
         // Apply dither effect
-        const m = [
-          [3, 1],
-          [0, 2],
-        ];
+        makeDithered(p);
 
-        p.loadPixels();
-        for (let i = 0; i < p.pixels.length; i += 4) {
-          let x = (i / 4 | 0) % normalizedWidth;
-          let y = (i / 4 / normalizedWidth | 0);
-          let thresh = m[x%2][y%2] * 20 + 60;
-          let pixel = (p.pixels[i] + p.pixels[i + 1] + p.pixels[i + 2]) / 3; // Pixel brightness as an average of the R,G,B values
-      
-          p.pixels[i] = pixel < thresh ? props.fgColor[0] : props.bgColor[0]; // Red
-          p.pixels[i + 1] = pixel < thresh ? props.fgColor[1] : props.bgColor[1]; // Green
-          p.pixels[i + 2] = pixel < thresh ? props.fgColor[2] : props.bgColor[2]; // Blue
-          p.pixels[i + 3] = 255; // Alpha
-        }
-
-        // Apply grid
-        const gridSpace = 4;
-        const rowLength = normalizedWidth * 4; // There are four entries for each pixel
-        for( let y = 0; y < p.pixels.length; y += rowLength * gridSpace ){
-          for (let x = y; x < y + rowLength; x+= 4 * gridSpace) {
-            p.pixels[x] = props.fgColor[0] // red;
-            p.pixels[x+1] = props.fgColor[1]; // green
-            p.pixels[x+2] = props.fgColor[2]; // blue;
-            p.pixels[x+3] = 255; // alpha
-          }
-        }
+        // Apply dot grid
+        generateDotGrid(p);
         
         p.updatePixels();
+
+        // Draw the cursor
+        // drawCursor(p);
       }
     });
+  }
+
+  const drawCursorTrail = (p) => {
+    // Add the current cursor position to the points list
+    if( drawing.current ){
+      points.current.push({
+        x: p.mouseX,
+        y: p.mouseY,
+        age: 0
+      });
+    }
+
+    // Loop through the points array and draw each one
+    let prevPoint = {};
+    p.strokeWeight(lineWidth.current);
+    p.strokeCap(p.ROUND);
+    points.current.forEach((point, i) => {
+      if( point.age <= pointMaxAge ){
+        // Draw a line connecting each point to the previous one
+        if( i > 0 ){
+          const value = 255 * (point.age / pointMaxAge);
+          p.stroke(value, value, value);
+          p.line(prevPoint.x, prevPoint.y, point.x, point.y);
+        }
+        prevPoint = point;
+
+      } else {
+        delete points.current[i]; // Delete the circle when it gets too old
+      }
+
+      point.age += 1;
+    });
+    p.filter(p.BLUR, lineWidth.current / 4);
+  }
+
+  // Apply the dither effect
+  // Based on Bayer Dithering by illus0r
+  // https://editor.p5js.org/illus0r/sketches/YkkcqhLmY
+  // p.updatePixels() must be called after calling this function
+  const makeDithered = (p) => {
+    const normalizedWidth = Math.floor(p.width / props.pixelDensity);
+    const m = [
+      [3, 1],
+      [0, 2],
+    ];
+
+    p.loadPixels();
+
+    for (let i = 0; i < p.pixels.length; i += 4) {
+      let x = (i / 4 | 0) % normalizedWidth;
+      let y = (i / 4 / normalizedWidth | 0);
+      let thresh = m[x%2][y%2] * 20 + 60;
+      let pixel = (p.pixels[i] + p.pixels[i + 1] + p.pixels[i + 2]) / 3; // Pixel brightness as an average of the R,G,B values
+  
+      p.pixels[i] = pixel < thresh ? props.fgColor[0] : props.bgColor[0]; // Red
+      p.pixels[i + 1] = pixel < thresh ? props.fgColor[1] : props.bgColor[1]; // Green
+      p.pixels[i + 2] = pixel < thresh ? props.fgColor[2] : props.bgColor[2]; // Blue
+      p.pixels[i + 3] = 255; // Alpha
+    }
+  }
+
+  // Generate the dot grid
+  // p.updatePixels() must be called after calling this function
+  const generateDotGrid = (p) => {
+    const normalizedWidth = Math.floor(p.width / props.pixelDensity);
+    const gridSpace = 8;
+    const rowLength = normalizedWidth * 4; // There are four entries for each pixel
+
+    for( let y = rowLength * gridSpace / 2; y < p.pixels.length; y += rowLength * gridSpace ){
+      for (let x = y + (gridSpace * 2); x < y + rowLength; x+= 4 * gridSpace) {
+        p.pixels[x] = props.fgColor[0] // red;
+        p.pixels[x+1] = props.fgColor[1]; // green
+        p.pixels[x+2] = props.fgColor[2]; // blue;
+        p.pixels[x+3] = 255; // alpha
+      }
+    }
+  }
+
+  const drawCursor = (p) => {
+    const cursorSize = 16;
+    p.strokeWeight(2);
+    p.strokeCap(p.PROJECT);
+    p.stroke(0, 0, 0, 255);
+    const mousePos = { // Round coordinates so pixels are always clear
+      x: 2 * Math.round(p.mouseX / 2) + 1,
+      y: 2 * Math.round(p.mouseY / 2) + 1,
+    }
+    p.line(mousePos.x, mousePos.y - (cursorSize / 2), mousePos.x, mousePos.y + (cursorSize / 2),); // Vertical line
+    p.line(mousePos.x- (cursorSize / 2), mousePos.y, mousePos.x + (cursorSize / 2), mousePos.y,); // Horizontal line
   }
   
   return (
