9401d7b828d23b264b6c37434db11257680e480a
Alex Berkowitz <berky93@gmail.com>
2024-10-24 15:10:23 -0500

    fix unused vars

--- app/components/BackButton/BackButton.jsx
+++ app/components/BackButton/BackButton.jsx
@@ -1,13 +1,10 @@
 'use client'
 
-import { useGlobalContext } from '@/GlobalContext';
 import Link from '@/components/Link/Link';
 
 import styles from "./backButton.module.scss";
 
 const BackButton = () => {
-  const globalContext = useGlobalContext();
-
   return (
     <Link
       className={styles.backButton}
