cd70c01ab9ab90efdd590f5ff5d7d8a1d8227287
Alex Berkowitz <berky93@gmail.com>
2024-11-15 14:42:37 -0600

    button updates

--- app/components/MDX/CodeHeader.jsx
+++ app/components/MDX/CodeHeader.jsx
@@ -1,32 +1,45 @@
 'use client'
 
+import { useState } from 'react';
 import Icon from '@/components/Icon/Icon';
 import Button from '@/components/Button/Button';
 
 import styles from './mdx.module.scss';
 
 const CodeHeader = (props) => {
+  const [success, setSuccess] = useState(false);
+  const successAnimDuration = 2; // Seconds
+
   const copyText = async function(e) {
     const text = e.target.closest('pre').querySelector('code').innerText;
     await navigator.clipboard.writeText(text);
+    setSuccess(true);
+    setTimeout(() => {
+      setSuccess(false)
+    }, successAnimDuration * 1000);
   }
 
   const copyButton = () => {
     return (
       <Button
-        className={styles.copyButton}
+        className={`${styles.copyButton} ${success ? styles.success : ''}`}
         onClick={(e) => copyText(e)}
         icon={true}
         aria-label="Copy"
         >
-        <Icon type="copy"/>
+        <Icon type="copy" />
       </Button>
     )
   }
   
   if( !!props.language ){
     return (
-      <div className={styles.codeHeader}>
+      <div
+        className={styles.codeHeader}
+        style={{
+          '--animDuration': `${successAnimDuration}s`
+        }}
+        >
         <p>{props.language}</p>
         {copyButton()}
       </div>
