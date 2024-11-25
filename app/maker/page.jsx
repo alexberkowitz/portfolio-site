6de4611a19ee2155ff5a4021b25e0f1cb49d31b7
Alex Berkowitz <berky93@gmail.com>
2024-11-25 11:45:16 -0600

    Fix apostrophes

--- app/maker/page.jsx
+++ app/maker/page.jsx
@@ -6,7 +6,7 @@ export default function Maker() {
     <main>
       <p style={{padding: 'calc(var(--pagePadding) * 2) 0', alignSelf: 'center'}}>
         <Text>
-          For all of the love that I hold for digital content creation, I also really love making real, physical products. I have an extensive history as an amateur maker & product designer. I've designed products for companies, such as the Mercury Knife for Kizer, as well as for myself. I'm well-versed in CAD/CAM, 3D printing, CNC machining, and a whole slew of other manufacturing processes ranging from DIY to industrial. 
+          For all of the love that I hold for digital content creation, I also really love making real, physical products. I have an extensive history as an amateur maker & product designer. I&lsquo;ve designed products for companies, such as the Mercury Knife for Kizer, as well as for myself. I&lsquo;m well-versed in CAD/CAM, 3D printing, CNC machining, and a whole slew of other manufacturing processes ranging from DIY to industrial. 
         </Text>
       </p>
       <ProjectsList type="maker"/>
